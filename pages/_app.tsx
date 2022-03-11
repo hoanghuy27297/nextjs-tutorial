d18d1139357cdfac34ceab80fc248390b21f0d90
Huy Hoang <hoanghuy27297@gmail.com>
2022-03-11 14:02:08 +1100

    added post components and layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
+import { Layout } from '../components'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {}, [])
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
