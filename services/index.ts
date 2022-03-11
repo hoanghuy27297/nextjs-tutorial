a68a0d46b9ede9ecfc6f5bc50aeb6d6909776271
Huy Hoang <hoanghuy27297@gmail.com>
2022-03-11 14:35:46 +1100

    added post widget and categories component

--- services/index.ts
+++ services/index.ts
@@ -3,7 +3,7 @@ import { request, gql } from 'graphql-request';
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
 export const getPosts = async () => {
-    const query = gql`
+  const query = gql`
         query MyQuery {
           postsConnection {
             edges {
@@ -34,7 +34,60 @@ export const getPosts = async () => {
         }
     `
 
-    const results = await request(graphqlAPI!, query);
+  const results = await request(graphqlAPI!, query);
 
-    return results.postsConnection.edges.map((edge) => ({...edge.node}));
+  return results.postsConnection.edges.map((edge) => ({ ...edge.node }));
+}
+
+export const getRecentPosts = async () => {
+  const query = gql`
+    query GetPostDetails {
+      posts(orderBy: createdAt_ASC, last: 3) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `
+
+  const results = await request(graphqlAPI!, query);
+
+  return results.posts;
+}
+
+export const getSimilarPosts = async () => {
+  const query = gql`
+    query GetPostDetails($slug: String!, $categories: [String!]) {
+      posts(where: {slug_not: $slug, AND: {categories_some: {slug_in $categories}}}, last: 3) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `
+
+  const result = await request(graphqlAPI!, query);
+
+  return result.posts;
+}
+
+export const getCategories = async () => {
+  const query = gql`
+    query GetCategories {
+      categories {
+        name,
+        slug
+      }
+    }
+  `
+
+  const results = await request(graphqlAPI!, query);
+
+  return results.categories;
 }
